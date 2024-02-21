8f5f9ab71bbd72ec7b07cb14329f0f3ece70aaca
Daniel <danielbyomujuni@gmail.com>
2024-02-20 22:20:14 -0500

    Revamped Landing Page

--- lib/views/Landing.dart
+++ lib/views/Landing.dart
@@ -17,57 +17,126 @@ class Landing extends StatefulWidget {
 }
 
 class _LandingState extends State<Landing> {
-
-
-
   @override
   Widget build(BuildContext context) {
     isAuthed().then((value) async => {
-        if (value) {
-              Navigator.push(context,
-                PageTransition(
-                  type: PageTransitionType.bottomToTop,
-                  duration: const Duration(milliseconds: 250),
-                  child: const Tracker(title: 'Neko Syn 2241 Beta',),
-                )
-              )
-        } else {
-        ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(
-            content: const Text('Unable to access server (Check your network or Contact your Administator)'),
-            showCloseIcon: true,
-            duration: const Duration(days: 2),
-            backgroundColor: Theme.of(context).colorScheme.error,
-          )
-        )
-        }
-      }
-    );
+          if (value)
+            {
+              Navigator.push(
+                  context,
+                  PageTransition(
+                    type: PageTransitionType.bottomToTop,
+                    duration: const Duration(milliseconds: 250),
+                    child: const Tracker(
+                      title: 'Neko Syn 2241 Beta',
+                    ),
+                  ))
+            }
+          else
+            {
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                content: const Text(
+                    'Unable to access server (Check your network or Contact your Administator)'),
+                showCloseIcon: true,
+                duration: const Duration(days: 2),
+                backgroundColor: Theme.of(context).colorScheme.error,
+              ))
+            }
+        });
 
+    return Scaffold(
+        body: Stack(children: <Widget>[
+      Positioned(
+          height: MediaQuery.of(context).size.height / 2,
+          child: ShaderMask(
+            shaderCallback: (rect) {
+              return const LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Colors.black, Colors.transparent],
+              ).createShader(
+                  Rect.fromLTRB(0, 0, rect.width, rect.height / 1.2));
+            },
+            blendMode: BlendMode.dstIn,
+            child: Image.asset(
+              'assets/banner.jpg',
+              height: 400,
+              fit: BoxFit.contain,
+            ),
+          )),
+      Positioned(
+          top: -20,
+          left: 20,
+          height: MediaQuery.of(context).size.height / 2,
+          child: Center(
+              child: Wrap(children: <Widget>[
+            Container(
+                clipBehavior: Clip.antiAlias,
+                decoration: BoxDecoration(
+                    borderRadius:
+                        BorderRadius.circular(15) // Adjust the radius as needed
+                    ),
+                child: Image(
+                  image: AssetImage('assets/Purductive.png'),
+                  width: MediaQuery.of(context).size.width / 3,
+                )),
+            SizedBox(width: 8.0),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Purductive",
+                    style: TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                        color: Theme.of(context).colorScheme.inverseSurface)),
+                Text("By Nekosyndicate",
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Theme.of(context).colorScheme.inverseSurface))
+              ],
+            )
+          ]))),
+    Positioned(
+    top: MediaQuery.of(context).size.height / 2,
+    left: 20,
+    height: MediaQuery.of(context).size.height / 2,
+    child: Column(children: [
+          FilledButton(
+              onPressed: () {
+                _settingModalCompanyBottomSheet(context);
+              },
+              child: const Text('Choose your Company')),
+
+          const SizedBox(height: 20),
+          FilledButton.tonal(
+              style: ButtonStyle(),
+              onPressed: () {},
+              child: const Text('Connect to Private Dedicated Server')),
+    ],)
+    )
+    ]));
+/*
     return Scaffold(
         body: Center(
             child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Spacer(flex: 5),
-          Container(
-              decoration: BoxDecoration(
-                borderRadius: const BorderRadius.all(Radius.circular(20)),
-                boxShadow: [
-                  BoxShadow(
-                    color: Theme.of(context).colorScheme.onBackground,
-                    spreadRadius: 5,
-                    blurRadius: 7,
-                    offset: const Offset(0, 3), // changes position of shadow
-                  ),
-                ],
-              ),
-              clipBehavior: Clip.hardEdge,
-              margin:
-                  const EdgeInsets.symmetric(vertical: 20.0, horizontal: 60.0),
-              child: const Wrap(children: <Widget>[
-                Image(image: AssetImage('assets/logo.png'))
-              ])),
+                  DecoratedBox(
+                  decoration: BoxDecoration (image: DecorationImage(image: AssetImage("Purductive"), fit: BoxFit.cover)),
+                  child: Container(decoration: BoxDecoration(borderRadius: const BorderRadius.all(Radius.circular(20)),
+                    boxShadow: [BoxShadow(color: Theme.of(context).colorScheme.onBackground, spreadRadius: 5, blurRadius: 7, offset: const Offset(0, 3), // changes position of shadow
+            ),
+          ],
+        ),
+        clipBehavior: Clip.hardEdge,
+        margin:
+        const EdgeInsets.symmetric(vertical: 20.0, horizontal: 60.0),
+        child: const Wrap(children: <Widget>[
+          Image(image: AssetImage('assets/logo.png'))
+        ])),
+                ),
           const Text("Neko Syndicate",
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 36.0)),
           const Text(
@@ -86,7 +155,7 @@ class _LandingState extends State<Landing> {
               child: const Text('Connect to Private Dedicated Server')),
                   Spacer(flex: 10),
                 ])
-        ));
+        ));*/
   }
 }
 
@@ -114,15 +183,15 @@ void _settingModalCompanyBottomSheet(context) {
                   leading: const Icon(Icons.phone_android),
                   title: const Text('Neko Syndicate 2241 Debug'),
                   onTap: () => {
-                    Navigator.pop(context),
-                  Navigator.push(
-                      context,
-                      PageTransition(
-                        type: PageTransitionType.bottomToTop,
-                        duration: const Duration(milliseconds: 250),
-                        child: Login(),
-                      ))
-                  }),
+                        Navigator.pop(context),
+                        Navigator.push(
+                            context,
+                            PageTransition(
+                              type: PageTransitionType.bottomToTop,
+                              duration: const Duration(milliseconds: 250),
+                              child: Login(),
+                            ))
+                      }),
             ],
           ),
         );
